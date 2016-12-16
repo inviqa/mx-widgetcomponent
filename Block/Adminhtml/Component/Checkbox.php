8807bf340cf8b7ba23fc07d260183f7b5cc219f9
Laszlo Kispal <lkispal@sessiondigital.com>
2016-12-16 15:26:08 +0100

    Add checkbox component

---
+++ Block/Adminhtml/Component/Checkbox.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace MX\WidgetComponent\Block\Adminhtml\Component;
+
+use Magento\Backend\Block\Template;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Data\Form\Element\Factory;
+
+/**
+ * Checkbox - useful for replacing Select boolean
+ *
+ * <data>
+ *     <item name="value" xsi:type="string">value</item>
+ * </data>
+ *
+ * @package MX\WidgetComponent\Block\Adminhtml\Component
+ */
+class Checkbox extends Template
+{
+    /**
+     * @var Factory
+     */
+    protected $elementFactory;
+
+    /**
+     * @param Context $context
+     * @param Factory $elementFactory
+     * @param array   $data
+     */
+    public function __construct(Context $context, Factory $elementFactory, $data = [])
+    {
+        $this->elementFactory = $elementFactory;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Prepare chooser element HTML
+     *
+     * @param AbstractElement $element Form Element
+     * @return AbstractElement
+     */
+    public function prepareElementHtml(AbstractElement $element)
+    {
+        $checkbox = $this->createCheckboxElement($element);
+
+        $element->setData(
+            'after_element_html',
+            $checkbox->getElementHtml()
+        );
+        $element->setValue(''); // Stop loading the value back for the parent element
+
+        return $element;
+    }
+
+    /**
+     * @param AbstractElement $baseElement
+     *
+     * @return AbstractElement
+     */
+    protected function createCheckboxElement(AbstractElement $baseElement)
+    {
+        $config = $this->_getData('config');
+
+        $checked = false;
+        if ($baseElement->getEscapedValue()) {
+            $checked = true;
+        }
+
+        $value = $baseElement->getName() . '_checkbox';
+        if (!empty($config['value'])) {
+            $value = $config['value'];
+        }
+
+        $checkbox = $this->elementFactory->create('checkbox', ['data' => $baseElement->getData()]);
+        $checkbox->setValue($value);
+        $checkbox->setIsChecked($checked);
+        $checkbox->setId($baseElement->getId());
+        $checkbox->setForm($baseElement->getForm());
+        if ($baseElement->getRequired()) {
+            $checkbox->addClass('required-entry');
+        }
+
+        return $checkbox;
+    }
+}
