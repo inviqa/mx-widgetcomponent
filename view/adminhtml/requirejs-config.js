346c0aeaea9c25f50f180e87022b8ce0a8af57ff
tkotosz <tkotosz@inviqa.com>
2016-12-17 20:36:30 +0100

    fix

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     map: {
         '*': {
             MXWidgetComponentImagePicker:     'MX_WidgetComponent/js/component/image-picker',
-            MXWidgetComponentSubWidget:     'MX_WidgetComponent/js/component/sub-widget'
+            MXWidgetComponentSubWidget:       'MX_WidgetComponent/js/component/sub-widget'
         }
     }
 };
